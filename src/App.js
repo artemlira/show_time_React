f6cf736a59e91be1ffe2d489d7a042558cbb8f3f
Lira Artem <artemlira84@gmail.com>
2022-07-21 21:45:47 +0300

    добавил вывод времени

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 
+function CurrentDate() {
+   return <h2>{new Date().toLocaleDateString()}</h2>;
+}
+
+function CurrentTime() {
+   return <h2>{new Date().toLocaleTimeString()}</h2>;
+}
+
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+   return (
+      <div className="App">
+         <CurrentDate />
+         <CurrentTime />
+      </div>
+   );
 }
 
 export default App;
